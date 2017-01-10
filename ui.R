2c123627954cc9cfed31a31c1a2d0c6860a2a017
nadiaadila <nadiaadilabtmuhammad@gmail.com>
2017-01-10 17:32:07 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(ggplot2)
+
+#setwd('C:/Users/saya/Desktop/DS/real kuiz/Developing Data Project/Week4/a2')
+dataset <- read.csv('mixtures.csv')
+
+fluidPage(
+  
+  titlePanel("Cements Explorer"),
+  
+  sidebarPanel(
+    
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+    
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+)
